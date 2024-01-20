5075242e115618cf023ee2c635042499a814bf89
kumar11jr <kumar11jr@gmail.com>
2024-01-20 21:44:55 +0530

    add: Banner component

---
+++ app/(dashboard)/_components/banner.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { useMutation } from "convex/react"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+
+
+interface BannerProps{
+    documentId: Id<"documents">
+}
+
+export const Banner =({
+    documentId 
+}:BannerProps)=>{
+    const router = useRouter()
+
+    const remove = useMutation(api.document.remove)
+
+    const onRemove=()=>{
+        const promise = remove({id:documentId})
+
+        toast.promise(promise,{
+            loading:"Deleting Note...",
+            success:"Note Deleted",
+            error:"Failed to delete note",
+        })
+        router.push("/dash")
+    }
+
+    return(
+        <div className="w-full bg-red-500 text-center">
+        
+        </div>
+    )
+}
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 
 interface NavbarProps{
@@ -24,8 +26,11 @@ export const Navbar =({
 
     if(document == undefined){
         return (
-            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center ">
-
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
+                <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         )
     }
@@ -42,9 +47,15 @@ export const Navbar =({
                 )}
                 <div className="flex items-center justify-between w-full">
                     <Title initialData= {document} />
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId = {document._id}  />
+                    </div>
                 </div>
 
             </nav>
+            {!document.isArchived && (
+                <Banner documentId={document._id} />
+            )}
         </>
     )
 }

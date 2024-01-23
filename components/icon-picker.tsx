1bed691861b251d7603d6c1eac13813f6be66866
kumar11jr <kumar11jr@gmail.com>
2024-01-23 19:29:09 +0530

    add:image and emoji picker

--- components/icon-picker.tsx
+++ components/icon-picker.tsx
@@ -29,9 +29,13 @@ export const IconPicker = ({onChange,children,asChild}:IconPickerProps)=>{
 
     return(
         <Popover>
-            <PopoverTrigger>
-                
+            <PopoverTrigger asChild={asChild} >
+                {children}
             </PopoverTrigger>
+            <PopoverContent className="p-0 w-full border-none shadow-none">
+                <EmojiPicker height={350} theme={theme} onEmojiClick={(data)=>onChange(data.emoji)} />
+
+            </PopoverContent>
         </Popover>
     )
 }
--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { Doc } from "@/convex/_generated/dataModel"
+import { IconPicker } from "./icon-picker";
+import { Button } from "./ui/button";
+import { ImageIcon, Smile, X } from "lucide-react";
 
 
 interface ToolbarProps{
@@ -13,10 +16,50 @@ export const Toolbar=({initialData,preview}:ToolbarProps)=>{
         <div className="pl-[54px] group relative">
             {!!initialData && !preview && (
                 <div className="flex items-center gap-x-2 group/icon pt-6">
-
-
+                    <IconPicker onChange={()=>{}}>
+                        <p className="text-6xl hover:opacity-75 transition">
+                            {initialData.icon}
+                        </p>
+                    </IconPicker>
+                    <Button
+                        onClick={()=>{}}
+                        className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
+                        variant="outline"
+                        size="icon"
+                    >
+                        <X className="h-4 w-4" />
+                    </Button>
                 </div>
             )}
+            {!!initialData.icon && preview && (
+                <p className="text-6xl ">
+                    {initialData.icon}
+                </p>
+            )}
+            
+            <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4 ">
+                {!initialData.icon && !preview && (
+                    <IconPicker asChild onChange={()=>{}}>
+                        <Button className="text-muted-foreground text-xs " variant="outline" size="sm"
+                        >
+                            <Smile className="h-4 w-4 mr-2" />
+                            Add Icon
+                        </Button>
+                    </IconPicker>
+                )}
+
+                {!initialData.coverImage && !preview && (
+                    <Button
+                        onClick={()=>{}}
+                        className="text-muted-foreground text-xs"
+                        variant="outline"
+                        size="sm"
+                    >
+                        <ImageIcon className="h-4 w-4 mr-2" />
+                    </Button>
+                )}
+
+            </div>
             
         </div>
     )
